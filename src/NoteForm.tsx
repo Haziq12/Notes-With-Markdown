981a6768f20ff6de7c8035256d521be28e1d528b
Haziq Naeem <haziqnaeem36@gmail.com>
2022-11-15 18:52:57 -0500

    create useRefs for title and markdown, pass onSubmit prop to NoteForm

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,17 +1,25 @@
 import { Form, Stack, Row, Col, Button } from "react-bootstrap"
 import { Link } from "react-router-dom";
 import CreateableReactSelect from "react-select/creatable"
+import { FormEvent, useRef } from "react" 
 
 
-export function NoteForm() {
+export function NoteForm({ onSubmit }) {
+  const titleRef = useRef<HTMLInputElement>(null)
+  const markdownRef = useRef<HTMLTextAreaElement>(null)
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required />
+              <Form.Control required ref={titleRef}/>
             </Form.Group>
           </Col>
           <Col>
@@ -23,7 +31,7 @@ export function NoteForm() {
         </Row>
         <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea" rows={15} />
+          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
