a65f0e3f3df327a4bdbfdb2d0aa5b0c343b90a96
Haziq Naeem <haziqnaeem36@gmail.com>
2022-11-15 17:52:06 -0500

    confifgure react router and bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App 
+export default App;
